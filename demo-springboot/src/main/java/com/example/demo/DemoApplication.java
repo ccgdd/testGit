b697c0734211ae679eb45e51b1f36fd0e863e4a8
ccgdd <35952779+ccgdd@users.noreply.github.com>
2019-09-06 18:24:12 +0800

    添加getRequestFactory()方法

--- demo-springboot/src/main/java/com/example/demo/DemoApplication.java
+++ demo-springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,11 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
+    
+    
+    
+    private ClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        public ClientHttpRequestFactory getRequestFactory() {
+        return this.requestFactory;
+    }
 }
